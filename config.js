387241623a129273d76b055cfd153afe834aed28
danu08877-prog <danu08877@gmail.com>
2025-08-06 00:35:11 +0530

    Update config.js

--- config.js
+++ config.js
@@ -7,7 +7,7 @@ function convertToBool(text, fault = 'true') {
 
 module.exports = {
 
-SESSION_ID: process.env.SESSION_ID === undefined ? '𝙽𝙰𝙳𝙴𝙴𝙽-𝙼𝙳=rcNEnBrR#DeNmXMCKTRaHQq_pGg5pfjTuQP-WXFSR9iENTZcy2hw' : process.env.SESSION_ID, 
+SESSION_ID: process.env.SESSION_ID === undefined ? '𝙽𝙰𝙳𝙴𝙴𝙽-𝙼𝙳=2MtzWAbB#Jl_-c-L82SHiMP9CbGwuCmOPUxdoJmEk49itAA5YN_Y' : process.env.SESSION_ID, 
 PORT: process.env.PORT === undefined ? '8000' : process.env.PORT, 
 SESSION_NAME: process.env.SESSION_NAME === undefined ? 'nadeenmd' : process.env.SESSION_NAME
 };
